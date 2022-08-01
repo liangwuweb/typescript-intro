58690b58515a4001cb9fbd322b4a619553cede0b
Liang Wu <liangwuweb@gmail.com>
2022-08-01 15:01:53 -0500

    Tuples and Enums

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let user = [1, 'Mosh'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-// Arrarys
-let numbers: number[] = [1,2,3];
-let numbers2: number[] = [];
-let numbers3 = [1,2,3];
+// Tuples -- fix length of array, each value is different type
+// useful for two values, key value pairs
+let user: [number, string] = [1, 'Mosh'];
 
-// auto complettion
-numbers.forEach(n => n.toExponential);
+// Enums
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size { Small, Medium, Large }
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
+
+
 
 
