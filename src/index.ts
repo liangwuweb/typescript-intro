419174782a92f993ea8c83c7b30a4434f4cf015d
Liang Wu <liangwuweb@gmail.com>
2022-08-02 10:55:25 -0500

    Type Aliases

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-// object
-let employee: {
+// Type Aliases
+type Employee = {
   readonly id: number,
   name:string,
   retire: (date: Date) => void
-} = { 
+} 
+
+
+
+let employee: Employee = { 
   id: 1, 
   name: 'Mosh', 
   retire: (date: Date) => {
