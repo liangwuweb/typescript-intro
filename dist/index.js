f0de4f4c6118da9d4bdbc242929fd64eaa271124
Liang Wu <liangwuweb@gmail.com>
2022-08-02 12:12:05 -0500

    optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,7 @@
 "use strict";
-function greet(name) {
-    if (name) {
-        console.log(name.toUpperCase());
-    }
-    else {
-        console.log('null or undefined detected! Please input valid value.');
-    }
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-greet(null);
+let customer = getCustomer(0);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-// nullable types
-function greet(name: string | null | undefined ) {
-  if (name) {
-    console.log(name.toUpperCase());
-  } else {
-    console.log('null or undefined detected! Please input valid value.')
-  }
-  
+// optional chaining
+type Customer = {
+  birthday: Date
 }
 
-greet(null);
+function getCustomer(id: number): Customer | null | undefined{
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+// if (customer !== null && customer !== undefined)
+//   console.log(customer.birthday);
+
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// optinal element access operator
+customer?.[0] // access customer only if it's not undefiened and null, otherwise return undefiened
+
+// optional call
+let log: any = null;
+log?.('hello');// call func only if it's not undefiened and null, otherwise return undefiened
